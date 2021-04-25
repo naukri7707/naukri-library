5f04250526d3ab2d14247061ed1fad39d7e35bad
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-04-25 22:28:10 +0800

    新增 ForkNameAttribute 使欄位在不同條件下顯示不同名稱

---
+++ UnityEditor/BetterAttribute/ForkNameAttributeDarwer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Reflection;
+using UnityEditor;
+
+namespace NaukriEditor
+{
+    [CustomPropertyDrawer(typeof(ForkNameAttribute), true)]
+    public class ForkNameAttributeDarwer : BetterPropertyDrawer
+    {
+        public override void OnGUILayout(SerializedProperty property, GUIContent label)
+        {
+            var attr = attribute as ForkNameAttribute;
+            BetterGUILayout.PropertyField(
+                    property,
+                    new GUIContent(
+                        CheckValue(property)
+                        ? attr.trueForkName
+                        : attr.falseForkName
+                        )
+                    );
+        }
+
+        private bool CheckValue(SerializedProperty property)
+        {
+            var attr = attribute as ForkNameAttribute;
+            var target = property.serializedObject.targetObject;
+            var type = target.GetType();
+            var value = type.GetField(attr.fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(target);
+            return value != null && value.Equals(attr.value);
+        }
+    }
+}
