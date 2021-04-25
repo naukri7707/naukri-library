aabd7d3da7bd40107086d028007606b69f2aa4b0
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-04-25 23:39:35 +0800

    新增 CastTo 透過快速反射來轉型

---
+++ CastTo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Naukri
+{
+    /// <summary>
+    /// 轉型成<see cref="TResult"/>
+    /// </summary>
+    /// <typeparam name="TResult">目標型態</typeparam>
+    public static class CastTo<TResult>
+    {
+        private static class Caster<TSource>
+        {
+            // 轉型用委派
+            public static readonly Func<TSource, TResult> cast = GetDelegate();
+
+            private static Func<TSource, TResult> GetDelegate()
+            {
+                var instanceParam = Expression.Parameter(typeof(TSource));
+                return
+                    Expression.Lambda<Func<TSource, TResult>>
+                    (
+                        Expression.ConvertChecked
+                        (
+                            instanceParam,
+                            typeof(TResult)
+                        ),
+                        instanceParam
+                    ).Compile();
+            }
+        }
+
+        /// <summary>
+        /// 將 <see cref="TSource"/> 轉型為 <see cref="TResult"/>
+        /// </summary>
+        /// <typeparam name="TSource">原始型態</typeparam>
+        public static TResult From<TSource>(TSource src)
+        {
+            return Caster<TSource>.cast(src);
+        }
+
+    }
+}
