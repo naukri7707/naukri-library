383275b9037cb731f985f0c392c80a3fbb301d7e
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-04-25 22:51:03 +0800

    新增 Singleton 以快速建立適用於 Unity 的單利模式

---
+++ UnityEditor/Singleton/SingletonAsset.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEditor;
+using Naukri;
+
+namespace NaukriEditor
+{
+    public abstract class SingletonAsset<T> : ScriptableObject where T : SingletonAsset<T>
+    {
+        private static readonly object loadLock = new object();
+
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance is null)
+                {
+                    lock (loadLock)
+                    {
+                        if (TryLoadAsset(out instance) || TryCreateAsset(out instance))
+                        {
+                            instance.OnSingletonLoaded();
+                        }
+                        else
+                        {
+                            throw new UnityException($"Create asset failed at \"{typeof(T).Name}\"");
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private static AssetPathAttribute Path
+        {
+            get
+            {
+                var type = typeof(T);
+                var res = type.GetCustomAttribute<AssetPathAttribute>();
+                if (res is null)
+                {
+                    throw new UnityException($"You have to define \"{nameof(AssetPathAttribute)}\" first at \"{type.Name}\"");
+                }
+                return res;
+            }
+        }
+
+        private static bool TryCreateAsset(out T asset)
+        {
+            asset = CreateInstance<T>();
+            var path = Path.assetPath;
+            UnityPath.CreateDirectoryInEditor(path);
+            AssetDatabase.CreateAsset(asset, path);
+            AssetDatabase.SaveAssets();
+            return asset != null;
+        }
+
+        private static bool TryLoadAsset(out T asset)
+        {
+            asset = UnityEditor.AssetDatabase.LoadAssetAtPath<T>(Path.assetPath);
+            return asset != null;
+        }
+
+        protected virtual void OnSingletonLoaded() { }
+    }
+}
