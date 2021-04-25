08d4d1afa1ec9d533e3284057c4d2501ca8cee65
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-04-25 22:28:54 +0800

    新增 DisplayWhenField(Not)EqualAttribute 使欄位在條件成立下才顯示

---
+++ UnityEditor/BetterAttribute/DisplayWhenFieldEqualAttributeDarwer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Reflection;
+using UnityEditor;
+
+namespace NaukriEditor
+{
+    [CustomPropertyDrawer(typeof(DisplayWhenFieldEqualAttribute), true)]
+    public class DisplayWhenFieldEqualAttributeDarwer : BetterPropertyDrawer
+    {
+        public override void OnGUILayout(SerializedProperty property, GUIContent label)
+        {
+            if(CheckValue(property))
+            {
+                BetterGUILayout.PropertyField(property);
+            }
+        }
+
+        private bool CheckValue(SerializedProperty property)
+        {
+            var isNot = true;
+            DisplayWhenFieldEqualAttribute attr = attribute as DisplayWhenFieldNotEqualAttribute;
+            if (attr is null)
+            {
+                isNot = false;
+                attr = attribute as DisplayWhenFieldEqualAttribute;
+            }
+            var target = property.serializedObject.targetObject;
+            var type = target.GetType();
+            var value = type.GetField(attr.fieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(target);
+            return value is null
+                ? false
+                : value.Equals(attr.value) ^ isNot;
+        }
+    }
+}
