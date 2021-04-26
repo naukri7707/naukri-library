4ef55ebbc3a61f009225fb41cdb7571913b4aba5
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-04-26 15:25:52 +0800

    新增 ScriptTemplates 和 NaukriBehaviour 作為自定義的腳本模板

---
+++ UnityEditor/ScriptTemplates/ScriptTemplates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace NaukriEditor
+{
+    public partial class ScriptTemplates
+    {
+        [MenuItem("Assets/Create/N# Script", false, 79)]
+        public static void CreateNaukriBehaviourTemplate()
+        {
+            ScriptFactory.Create("NaukriBehaviour.cs", "NewBehaviourScript.cs");
+        }
+    }
+}
