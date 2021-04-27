83a91b718e7d7190d9ef157c6624604a69f52f99
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-04-28 01:23:07 +0800

    新增 AwaitCoroutine 使 async-await 支援協程等候

---
+++ Unity/AwaitCoroutine/InstructionAwaiter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Naukri.AwaitCoroutine
+{
+    public static class InstructionAwaiter
+    {
+        public static YieldInstructionAwaiter GetAwaiter(this WaitForEndOfFrame instruction)
+        {
+            return new YieldInstructionAwaiter(instruction);
+        }
+
+        public static YieldInstructionAwaiter GetAwaiter(this WaitForFixedUpdate instruction)
+        {
+            return new YieldInstructionAwaiter(instruction);
+        }
+
+        public static YieldInstructionAwaiter GetAwaiter(this WaitForSeconds instruction)
+        {
+            return new YieldInstructionAwaiter(instruction);
+        }
+
+        public static CustomYieldInstructionAwaiter GetAwaiter(this WaitForSecondsRealtime instruction)
+        {
+            return new CustomYieldInstructionAwaiter(instruction);
+        }
+
+        public static CustomYieldInstructionAwaiter GetAwaiter(this WaitUntil instruction)
+        {
+            return new CustomYieldInstructionAwaiter(instruction);
+        }
+
+        public static CustomYieldInstructionAwaiter GetAwaiter(this WaitWhile instruction)
+        {
+            return new CustomYieldInstructionAwaiter(instruction);
+        }
+    }
+}
---
+++ Unity/AwaitCoroutine/YieldInstructionAwaiter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using System.Collections;
+
+namespace Naukri.AwaitCoroutine
+{
+    public struct YieldInstructionAwaiter : INotifyCompletion
+    {
+        public bool IsCompleted { get; private set; }
+
+        private readonly YieldInstruction instruction;
+
+        private Action continuation;
+
+        public YieldInstructionAwaiter(YieldInstruction instruction)
+        {
+            IsCompleted = false;
+            this.instruction = instruction;
+            continuation = null;
+        }
+
+        private IEnumerator AwaitCoroutine
+        {
+            get
+            {
+                yield return instruction;
+                continuation?.Invoke();
+            }
+        }
+
+        public void GetResult() { }
+
+        public void OnCompleted(Action continuation)
+        {
+            this.continuation = continuation;
+            AwaitCoroutineManager.StartCoroutineAwaiter(AwaitCoroutine);
+        }
+    }
+}
