e0072820f7392215f37d14e62d0ccb4d61e6a7f0
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-07-20 03:35:45 +0800

    新增 SceneObject 以模擬存取 SceneAsset

---
+++ Unity/SceneManagement/SceneObject.cs
@@ -0,0 +1,28 @@
+﻿using Naukri;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Naukri.Unity.SceneManagement
+{
+    [Serializable]
+    public class SceneObject
+    {
+
+#if UNITY_EDITOR
+        [SerializeField]
+        private UnityEngine.Object sceneAsset;
+#endif
+
+        [SerializeField]
+        private string sceneName = "";
+
+        public string Name => sceneName;
+
+        public static implicit operator string(SceneObject sceneField)
+        {
+            return sceneField.sceneName;
+        }
+    }
+}
