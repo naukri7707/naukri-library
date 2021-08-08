fc3486ca6cdf8b12610ee9537534bbc1f0c0891c
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-08 13:58:53 +0800

    新增 PropertyUsage 以自定義 ObjectField 之 Object Selector 可選取目標

---
+++ UnityEditor/BetterAttribute/PropertyUsageAttributeDrawer.cs
@@ -0,0 +1,25 @@
+﻿using Naukri.Unity.BetterAttribute;
+using Naukri.UnityEditor.BetterAttribute.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace Naukri.UnityEditor.BetterAttribute
+{
+    [CustomPropertyDrawer(typeof(PropertyUsageAttribute))]
+    public class PropertyUsageAttributeDrawer : BetterPropertyDrawer
+    {
+        public override void OnGUILayout(SerializedProperty property, GUIContent label)
+        {
+            var attr = attribute as PropertyUsageAttribute;
+            LayoutContainer(
+              () => property.objectReferenceValue = EditorGUI.ObjectField(
+                  position,
+                  property.displayName,
+                  property.objectReferenceValue,
+                  attr.type,
+                  attr.allowSceneObject),
+              EditorGUI.GetPropertyHeight(property)
+              );
+        }
+    }
+}
