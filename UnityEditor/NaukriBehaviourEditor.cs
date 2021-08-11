a156be369193abddf37fe3590a02f64bc53490bf
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-11 22:22:16 +0800

    新增 NaukriBehaviour 綁定 BetterInspector 和 MonoBehaviour

---
+++ UnityEditor/NaukriBehaviourEditor.cs
@@ -0,0 +1,17 @@
+﻿using Naukri.Unity;
+using Naukri.UnityEditor.BetterInspector.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace Naukri.UnityEditor
+{
+    [CustomEditor(typeof(NaukriBehaviour), true)]
+    public class NaukriBehaviourEditor : BetterInspectorEditor
+    {
+
+    }
+}
