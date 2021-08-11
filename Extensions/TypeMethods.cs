ca999d315a524c0ade96c777e79bec3b54feea11
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-12 03:18:32 +0800

    新增 IsSubclassOfRawGeneric() 檢查是否為某原生泛型的子類別

---
+++ Extensions/TypeMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Naukri.Extensions
+{
+    public static class TypeMethods
+    {
+        public static bool IsSubclassOfRawGeneric(this Type self, Type generic)
+        {
+            while (self != null && self != typeof(object))
+            {
+                if(self.IsGenericType && generic == self.GetGenericTypeDefinition())
+                {
+                    return true;
+                }
+                self = self.BaseType;
+            }
+            return false;
+        }
+    }
+}
