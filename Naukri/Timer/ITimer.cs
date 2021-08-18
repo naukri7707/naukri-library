f295d6440e3150da1b841b183bae09b223a54c2e
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-18 21:15:06 +0800

    新增 Timer 以建立正數器或倒數器

---
+++ Naukri/Timer/ITimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Events;
+
+namespace Naukri.Timer
+{
+    public interface ITimer
+    {
+        public float AlertTime { get; }
+
+        public UnityEvent OnAlert { get; }
+
+        public float Counter { get; }
+
+        public bool IsCounting { get; }
+
+        public void Play();
+
+        public void Pause();
+
+        public void Stop();
+
+        public void Reset();
+
+        public void Restart();
+
+        public void Tick();
+    }
+}
