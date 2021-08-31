c546b117bf7a9bac706ada277fff5a96aabfb637
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-31 16:20:39 +0800

    新增 LongFlag 將 long 欄位繪製為特定的 LongFlag

---
+++ Naukri/BetterAttribute/LongFlagAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Naukri.BetterAttribute
+{
+    public class LongFlagAttribute : PropertyAttribute
+    {
+        public readonly Type enumType;
+
+        public LongFlagAttribute(Type enumType)
+        {
+            this.enumType = enumType;
+            order = 10000;
+        }
+    }
+}
