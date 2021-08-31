5bf790db9e0592b710c0a154f85127e993ab47ee
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-31 16:10:19 +0800

    新增 Scope 結構用來明確區域進入及離開時的參數變化

---
+++ Naukri/Scopes.cs
@@ -0,0 +1,19 @@
+﻿using Naukri.Scopes;
+
+namespace Naukri
+{
+    public delegate ref T RefGetter<T>();
+
+    public static class Scope
+    {
+        public static SetValueScope<T> SetValue<T>(RefGetter<T> refGetter, T enterValue, T leaveValue)
+        {
+            return new SetValueScope<T>(refGetter, enterValue, leaveValue);
+        }
+
+        public static TempReplaceScope<T> TempReplace<T>(RefGetter<T> refGetter, T replaceValue)
+        {
+            return new TempReplaceScope<T>(refGetter, replaceValue);
+        }
+    }
+}
