44660cd0ead89a3844c98555234eacf4f3a96331
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-31 16:09:15 +0800

    新增 Heap 結構

---
+++ Naukri/Collections/Generic/MinHeap.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Naukri.Collections.Generic
+{
+    public class MinHeap<T> : Heap<T> where T : IComparable<T>, IEquatable<T>
+    {
+        public T Max
+        {
+            get => Valley;
+            set => Valley = value;
+        }
+
+        public T Min
+        {
+            get => Peak;
+            set => Peak = value;
+        }
+
+        public MinHeap() : base() { }
+
+        public MinHeap(int capacity) : base(capacity) { }
+
+        public MinHeap(IEnumerable<T> collection) : base(collection) { }
+
+        public T[] GetAllMaxValues() => GetAllValleyValues();
+
+        public T[] GetAllMinValues() => GetAllPeakValues();
+
+        protected override int OnCompare(T lhs, T rhs)
+        {
+            return rhs.CompareTo(lhs);
+        }
+    }
+}
