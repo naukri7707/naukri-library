d9a61e780a5ab9e130feeef901147a519573f502
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-31 16:08:54 +0800

    新增 常用擴充函式

---
+++ Naukri/Extensions/UnityMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Naukri.Extensions
+{
+    public static class UnityMethods
+    {
+        public static T GetOrAddComponent<T>(this Component self) where T : Component
+        {
+            return GetOrAddComponent<T>(self.gameObject);
+        }
+
+        public static T GetOrAddComponent<T>(this GameObject self) where T : Component
+        {
+            return self.TryGetComponent<T>(out var comp) ? comp : self.AddComponent<T>();
+        }
+    }
+}
