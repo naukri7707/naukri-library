12b3097c4526ad2bb15837cf7bb4f15ce16510ce
Naukri <50033668+naukri7707@users.noreply.github.com>
2021-08-31 16:21:26 +0800

    新增 DisplayWhenHasFlag 在欄位擁有特定旗標時顯示

---
+++ Naukri/BetterAttribute/DisplayWhenHasFlagAttribute.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Naukri.BetterAttribute
+{
+    public class DisplayWhenHasFlagAttribute : PropertyAttribute
+    {
+        public readonly string fieldName;
+
+        public readonly Flag flag;
+
+        public DisplayWhenHasFlagAttribute(string fieldName, Flag flag)
+        {
+            order = -10000;
+            this.fieldName = fieldName;
+            this.flag = flag;
+        }
+    }
+}
---
+++ Naukri/NaukriEditor/BetterAttribute/DisplayWhenHasFlagDrawer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Naukri;
+using Naukri.BetterAttribute;
+using Naukri.Extensions;
+using NaukriEditor.BetterAttribute.Core;
+using UnityEditor;
+using UnityEngine;
+
+namespace NaukriEditor.BetterAttribute
+{
+    [CustomPropertyDrawer(typeof(DisplayWhenHasFlagAttribute), true)]
+    public class DisplayWhenHasFlagDrawer : BetterPropertyDrawer
+    {
+        public override IEnumerable<BetterGUIWrapper> OnGUILayout(SerializedProperty property, GUIContent label, bool isOnGUI)
+        {
+            if (!CheckValue(property)) yield return BetterGUILayout.EmptyField(0);
+        }
+
+        private bool CheckValue(SerializedProperty property)
+        {
+            var attr = attribute as DisplayWhenHasFlagAttribute;
+            var target = property.serializedObject.targetObject;
+            var value = fieldInfo.GetValue(target);
+            return attr.flag.HasFlag((Flag)value);
+        }
+    }
+}
