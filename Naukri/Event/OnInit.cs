6968a27d0c4b92253f7c0fb5daabe12506316521
Naukri <naukri7707@gmail.com>
2021-12-01 21:40:21 +0800

    新增 Naukri.Event 快速註冊 MonoBehaviour 關鍵事件

---
+++ Naukri/Event/OnInit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Naukri.Event
+{
+    public class OnInit : MonoBehaviour
+    {
+        public UnityEvent onAwake;
+        public UnityEvent onStart;
+
+        protected virtual void Awake() => onAwake.Invoke();
+        protected virtual void Start() => onStart.Invoke();
+    }
+}
